f936adba2be113ef2b1d392d8c2f735babd5888f
Matthew Cross <matthewcross1017@gmail.com>
2021-11-20 01:53:50 -0500

    Start database

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments(
+ id INT,
+ name VARCHAR(30),
+ PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+id INT,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT, 
+PRIMARY KEY(id),
+);
+
+CREATE employee(
+id INT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT,
+PRIMARY KEY(id),
+REFERENCE()
+);
--- server.js
+++ server.js
@@ -1,1 +1,33 @@
+const express  = require('express')
+const mysql = require('mysql2');
+const inquirer = require('inquirer')
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.urlencoded({extended: false}));
+const 
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Rootroot',
+        database : 'employee_db'
+
+
+    },
+    console.log('connected to employee_db')
+);
+
+const startQuestions = [
+    {
+        type: "list",
+        message: "What would you like to do",
+        choices: ["View All Employees", "Update Employee Role", "View all Roles", "Add Role", "View All Departments", "Add Department", "View All Employees", "Quit"],
+         name: "menu"
+    },
+]
+  const departmentQuestions = [
+      {
+        
+      },
+  ]
