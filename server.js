b224d532a971e7aab1a7d2c78c07eb66fc1b26b7
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-11-22 14:40:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,14 +20,63 @@ const db = mysql.createConnection(
 
 const startQuestions = [
     {
-        type: "list",
-        message: "What would you like to do",
-        choices: ["View All Employees", "Update Employee Role", "View all Roles", "Add Role", "View All Departments", "Add Department", "View All Employees", "Quit"],
-         name: "menu"
+       type: "list",
+       message: "What would you like to do?",
+       choices: ["View All Employees", "Update Employee Role", "View all Roles", "Add Role", "View All Departments", "Add Department", "View All Employees", "Quit"],
+       name: "menu"
     },
-]
-  const departmentQuestions = [
+    ]
+  const addDepartment = [
       {
-        
+       type: "input",
+       message: "What is the name of the department?",
+       name: "deptName"
       },
-  ]
+    ]
+  const addRole = [
+      {
+       type: "input",
+       message: "What is the name of the role?",
+       name: "roleName"
+      },
+      {
+       type: "input",
+       message: "What is the salary of the role?",
+       name: "roleSalary"
+      },
+       {
+        type: "list",
+        message: "Which department does the role belong to?",
+        choices: ["Test1", "Test2"]
+    ]
+  const addEmployee = [
+       {
+       type: "input",
+       message: "What is the employee's first name?",
+       name: "employeeFirst"
+      },
+       {
+       type: "input",
+       message: "What is the employee's last name?",
+       name: "employeeLast"
+      },
+      {
+      type: "list",
+      message: "What is the employee's role?",
+      choices: [""]
+      name: "employeeRole"
+      },
+      {type : "input",
+       message: "Who is the employee's manager",
+       name: "employeeManager"
+      },
+   ]
+  const updateEmployee =[
+      {type: "list",
+       message: "which employee's role do you want to update?",
+       choices: [""]
+      },
+      {type: "list",
+       message: "Which role do you want to assgin the selected emoloyee?",
+       choices: [""]
+   ]
