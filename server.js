472c78e6266950a1cd571796d1c6e6e08ab5b753
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-11-22 16:25:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ const db = mysql.createConnection(
         user: 'root',
         password: 'Rootroot',
         database : 'employee_db'
-
-
     },
     console.log('connected to employee_db')
 );
@@ -22,17 +20,17 @@ const startQuestions = [
     {
        type: "list",
        message: "What would you like to do?",
-       choices: ["View All Employees", "Update Employee Role", "View all Roles", "Add Role", "View All Departments", "Add Department", "View All Employees", "Quit"],
+       choices: ["View All Employees", "Add Employee", "Update Employee Role", "View all Roles", "Add Role", "View All Departments", "Add Department", "View All Employees", "Quit"],
        name: "menu"
     },
-    ]
+    ];
   const addDepartment = [
       {
        type: "input",
        message: "What is the name of the department?",
        name: "deptName"
       },
-    ]
+    ];
   const addRole = [
       {
        type: "input",
@@ -48,7 +46,8 @@ const startQuestions = [
         type: "list",
         message: "Which department does the role belong to?",
         choices: ["Test1", "Test2"]
-    ]
+       }
+    ];
   const addEmployee = [
        {
        type: "input",
@@ -63,20 +62,58 @@ const startQuestions = [
       {
       type: "list",
       message: "What is the employee's role?",
-      choices: [""]
+      choices: [""],
       name: "employeeRole"
       },
       {type : "input",
        message: "Who is the employee's manager",
        name: "employeeManager"
       },
-   ]
-  const updateEmployee =[
+   ];
+  const updateEmployee = [
       {type: "list",
-       message: "which employee's role do you want to update?",
+       message: "Which employee's role do you want to update?",
        choices: [""]
       },
       {type: "list",
        message: "Which role do you want to assgin the selected emoloyee?",
        choices: [""]
-   ]
+      }
+   ];
+       function menuPrompt(){
+      inquirer.prompt(startQuestions).then((answers) => {
+          switch (answers.menu){
+           case "View all Employees":
+                  db.query('SELECT * FROM employees', function (err, results) {
+                  console.log(results)
+                  });
+           break;
+               case "View all Roles":
+                   db.query('SELECT * FROM roles, function (err, results) {
+                   console.log(results);
+                  });
+           break;
+               case "View all Departments":
+                    db.query('SELECT * FROM departments, function (err, results){
+                    console.log(results);
+                  });
+            break;
+                case "Add Employee":
+                inquirer.prompt(addEmployee).then((answers) => {
+                db.query('INSERT INTO employee {first_name: ${employeeFirst}, last_name: ${employeeLast} role
+                   });
+            break;   
+              case "Update Employee Role":
+                  inquirer.prompt(updateEmployee).then((answers) =>{
+                updateData = {
+                  updateEmployees(answers);
+             break;
+            });
+// function displayEmployees(){
+//     db.query('SELECT * FROM employees', function (err, results) {
+//   console.log(results);
+// });
+
+function updateEmployees(){
+db.query('INSERT INTO role{}
+});
